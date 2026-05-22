b1119c6568b84f9c5a48f6555e5d008cc9a99b83
NEwa-05 <mageekbox@gmail.com>
2026-05-22 19:33:19 +0200

    test

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # t37-repro-13208
-Reproduction configuration for traefik issue 13208
+
+Reproduction configuration for Traefik issue 13208
+
+## Create cluster
+
+```shell
+sudo k3d cluster create k8s-370 --port 80:80@loadbalancer --port 443:443@loadbalancer --k3s-arg "--disable=traefik@server:0"
+k3d kubeconfig get k8s-370 > .kubeconfig
+chmod 600 .kubeconfig
+```
+
+## load variables
+
+```shell
+source .env
+```
+
+## Deploy Traefik
+
+### Create Traefik NS
+
+```bash
+kubectl create ns traefik
+```
+
+### Create secret from cert file
+
+```bash
+kubectl create secret tls wildcard-mageekbox --namespace traefik --cert=.lego/certificates/${CLUSTERNAME}.${DOMAINNAME}.crt --key=.lego/certificates/${CLUSTERNAME}.${DOMAINNAME}.key
+```
+
+### Install with Helm
+
+```shell
+helm upgrade --install traefik traefik/traefik --create-namespace --namespace traefik --values ./traefik/values.yaml
+```
+
+### Add Dashboard
+
+```shell
+envsubst < ./traefik/dashboard.yaml | kubectl apply -f -
+```
+
+## deploy app
+
+```shell
+envsubst < ./whoami/whoami.yaml | kubectl apply -f -
+```
---
+++ reproduce.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+export startTime=$(date +%s.%N)
+
+
+function rolling {
+kubectl rollout restart -n whoami deploy whoami
+ATTEMPTS=0
+ROLLOUT_STATUS_CMD="kubectl rollout status -n whoami deploy whoami"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+  $ROLLOUT_STATUS_CMD
+  ATTEMPTS=$((attempts + 1))
+  sleep 10
+done
+}
+
+function checksvcip {
+kubectl -n whoami get endpointslices \
+  -l kubernetes.io/service-name=whoami \
+  -o json \
+  | jq -r '.items[].endpoints[] | select(.conditions.ready==true) | .addresses[]' \
+  | sort -u > svcepips.txt
+}
+
+function checkstrsvcip {
+curl -s https://dashboard.localdemo.mageekbox.eu/api/http/services | jq '.[] | select(.name | startswith("whoami-whoami")) | .loadBalancer.servers[].url'  \
+  | sed -E 's/.*http:\/\/([0-9.]+):[0-9]+.*/\1/' \
+  | sort -u > traefik-ipused.txt
+}
+
+function checktrlogs {
+    checktime=$(date +%s.%N)
+    difftime=`printf "%.0f" $(( (checktime - startTime) ))`
+    echo $difftime
+    kubectl -n traefik logs -l app.kubernetes.io/instance=traefik-traefik --since=${difftime}s \
+    grep 'whoami' \
+  | grep -oE '"url":"http://[0-9.]+:[0-9]+"' \
+  | sed -E 's/.*http:\/\/([0-9.]+):[0-9]+.*/\1/' \
+  | sort -u > trlog-ipused.txt
+}
+
+
+rolling
+checksvcip
+sleep 2
+checkstrsvcip
+checktrlogs
+comm -23 traefik-ipused.txt svcepips.txt
+comm -23 trlog-ipused.txt svcepips.txt
